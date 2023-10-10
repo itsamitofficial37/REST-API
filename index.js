704a8a2d0b86eef9acf15eab94886b54fb189cbe
itsamitofficial37 <amitofficial37@gmail.com>
2023-10-10 10:39:34 +0530

    Updated

--- index.js
+++ index.js
@@ -1,50 +1,85 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
+
 const app = express();
 const path = require("path");
 const port = 3000;
 
-app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get("/posts",(req,res)=> {
-    res.render("index.ejs",{posts});
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
 });
 
-app.get("/posts/new",(req,res)=> {
-    res.render("new.ejs");
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
 });
 
-app.post("/posts",(req,res) => {
-     
-    let {username,content} = req.body;
-    posts.push({username,content});
-    res.redirect("/posts")
-})
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
+});
 
-app.listen(port ,()=> {
-    console.log("server is start succesfully");
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res)=> {
+    let { id } = req.params;
+     posts = posts.filter((p) => id !== p.id);
+     res.redirect("/posts");
 })
+app.listen(port, () => {
+  console.log("server is start succesfully");
+});
 
 let posts = [
-    {
-        username : "amitsingh",
-        content : "Hard work is the key of success",
-    },
-    {
-        username : "vinitsingh",
-        content : "Education is the weapon you can change the world through it",
-    },
-    {
-        username : "kavitasingh",
-        content : "For changing in life , we have to sometimes go outside the home",
-    },
-    {
-        username : "ugarasensingh",
-        content : "If you find right people who supports you in every condition , never leave this person",
-    }
-]
+  {
+    id: uuidv4(),
+    username: "amitsingh",
+    content: "Hard work is the key of success",
+  },
+  {
+    id: uuidv4(),
+    username: "vinitsingh",
+    content: "Education is the weapon you can change the world through it",
+  },
+  {
+    id: uuidv4(),
+    username: "kavitasingh",
+    content: "For changing in life , we have to sometimes go outside the home",
+  },
+  {
+    id: uuidv4(),
+    username: "ugarasensingh",
+    content:
+      "If you find right people who supports you in every condition , never leave this person",
+  },
+];
